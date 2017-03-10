d9abb9dbefa590117bad912dbf9bbec520291f99
MARK HARRINGTON( 6-10-2016 15-00 <danu7_hm@danu7>
2017-03-10 18:11:50 +0000

    got some basic user shenanigan running

--- Waddle/client/main.js
+++ Waddle/client/main.js
@@ -4,25 +4,29 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 Template.navbar.onCreated(function navbarOnCreated() {
-  this.userType = new ReactiveVar("navAnon");
+  Session.set('isLecturer', false);
 });
 
 Template.navbar.helpers({
-  nav: function() {
-    return Template.instance().userType.get();
-  },
+	loggedIn() {
+		return !!Meteor.user();
+	},
+	isLecturer() {
+		return Session.get('isLecturer');
+	},
 });
 
 Template.navbar.events({
   'click .studentTest'(event, instance) {
-    instance.userType.set("navStudent");
+    Session.set('isLecturer', false);
   },
   'click .lecturerTest'(event, instance) {
-    instance.userType.set("navLecturer");
+    Session.set('isLecturer', true);
   },
-  'click .anonTest'(event, instance) {
-    instance.userType.set("navAnon");
+  'click .logout'(event, instance) {
+    Meteor.logout();
   },
+
 });
 
 
